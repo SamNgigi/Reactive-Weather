acc8ef23a3ddeac6c017f94f09eeeea2c0bbc103
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-04-07 10:25:28 +0300

    Added form to Form.js

--- reactive-weather/src/App.js
+++ reactive-weather/src/App.js
@@ -7,8 +7,21 @@ import React from "react";
 import Titles from "./components/Titles"
 import Form from "./components/Form";
 import Weather from "./components/Weather"
+
+// Open weather api key
+const API_KEY = "b5193b7df56e40afc55403b658e9f493";
+
 // We create an instance of App that is extending the React.Component object that lives in the node modules.
 class App extends React.Component {
+  // Custom method to communicate with api.
+  getWeather= async () => {
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}&units=metric`)
+
+    const response = await api_call.json();
+
+    console.log(response);
+  }
+
   // The render method is a build in React method that returns/displays Jsx.
   render() {
     // Jsx is kinda like a mixture of Js login and html, then the babel compiler converts code into normal readerble client code.

45879ab309bbb6223d402984b7e6e44a3bac86b9
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-04-07 11:21:38 +0300

    Displaying response data successfully

--- reactive-weather/src/App.js
+++ reactive-weather/src/App.js
@@ -12,38 +12,78 @@ import Weather from "./components/Weather"
 // Open weather api key
 const API_KEY = "b5193b7df56e40afc55403b658e9f493";
 
+
 // We create an instance of App that is extending the React.Component object that lives in the node modules.
 class App extends React.Component {
+
+
+  // State is an object that lives within a component and is responsible for changes within a component
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined,
+  }
+
   // Custom method to communicate with api.
   getWeather= async (event) => {
+
     // We prevent the default submition that occurs on is it click or submit event.
     // It seems that this event.prevent default is indicative of single paged applications
     event.preventDefault();
+
     // Grabbing the values entered into our forms.
     const city = event.target.elements.city.value;
     const country = event.target.elements.country.value;
+
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`)
 
     const response = await api_call.json();
 
     console.log(response);
+
+    // Displaying the response onto the browser.
+    this.setState({
+      temperature: response.main.temp,
+      city: response.name,
+      country: response.sys.country,
+      humidity: response.main.humidity,
+      description: response.weather[0].description,
+      error: ''
+    })
   }
 
   // The render method is a build in React method that returns/displays Jsx.
   render() {
+
     // Jsx is kinda like a mixture of Js login and html, then the babel compiler converts code into normal readerble client code.
     return (
-      // Jsx can only return one parent element. Anything outseid the parent element would not be read.
+
+      // Jsx can only return one parent element. Anything outside the parent element would not be read.
       <div>
         <Titles />
-        <Weather />
+
         {/* We use react props that are like html attributes that enable us to add functionalities to elements.
 
         In this case we want our getWeather function to be called on submition of our form.  */}
+
         <Form getWeather={this.getWeather} />
+
+        <Weather
+          temperature = {this.state.temperature}
+          city = {this.state.city}
+          country = {this.state.country}
+          humidity = {this.state.humidity}
+          description = {this.state.description}
+          error = {this.state.error}
+        />
+
       </div>
     );
   }
 }
+
 // We need to export the App instance so that other files can export
 export default App;
--- reactive-weather/src/components/Weather.js
+++ reactive-weather/src/components/Weather.js
@@ -4,7 +4,13 @@ class Weather extends React.Component {
   render(){
     return(
         <div>
-          <h4>Weather Component</h4>
+          <h4>Weather Component</h4><br/>
+          <p>{this.props.country}</p><br/>
+          <p>{this.props.city}</p><br/>
+          <p>{this.props.temperature}</p><br/>
+          <p>{this.props.humidity}</p><br/>
+          <p>{this.props.description}</p><br/>
+          <p>{this.props.error}</p><br/>
         </div>
     );
   }
